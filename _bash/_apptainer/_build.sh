8256ced45323d1dd7657129eeeeedc360a15c6f6
Lucas Nell <lucnell@gmail.com>
2025-09-05 12:40:54 -0400

    added bedtools to apptainer

--- _bash/_apptainer/_build.sh
+++ _bash/_apptainer/_build.sh
@@ -5,6 +5,15 @@
 #'
 
 
+#'
+#' If necessary, to send all these scripts to the cluster from your machine:
+#'
+#' cd ~/GitHub/Wisconsin/tany_time/_bash/_apptainer
+#' scp *.yml helpers.sh *.def *.py lnell@ap2001.chtc.wisc.edu:/home/lnell/_apptainer/
+#'
+
+
+
 export IN_DEF=tany_time.def
 export OUT_SIF=tany_time.sif
 
